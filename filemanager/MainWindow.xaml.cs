97b329d0fd11ae9fe8c8a1e0fd843e31aaeb9713
Cmoild <cold17748@gmail.com>
2024-02-26 23:28:31 +0300

    Добавьте файлы проекта.

---
+++ filemanager/MainWindow.xaml.cs
@@ -0,0 +1,37 @@
+﻿using System.IO;
+using System.Runtime.InteropServices;
+using System.Text;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace filemanager
+{
+    
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+            AllocConsole();
+            foreach (DriveInfo drive in drives)
+            {
+                Console.WriteLine(drive.Name);
+            }
+        }
+
+        DriveInfo[] drives = DriveInfo.GetDrives();
+
+        [DllImport("kernel32.dll", SetLastError = true)]
+        [return: MarshalAs(UnmanagedType.Bool)]
+        static extern bool AllocConsole();
+    }
+
+
+}
