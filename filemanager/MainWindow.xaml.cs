985577cf7939d1f939e0964e2737c4a2f65332f0
Cmoild <cold17748@gmail.com>
2024-02-29 02:49:06 +0300

    список дисков доделан

--- filemanager/MainWindow.xaml.cs
+++ filemanager/MainWindow.xaml.cs
@@ -10,34 +10,64 @@ using System.Windows.Media;
 using System.Windows.Media.Imaging;
 using System.Windows.Navigation;
 using System.Windows.Shapes;
+using System.Xml.Linq;
 
 namespace filemanager
 {
-    
+    //TODO: список каталогов доделать, перемещение по папкам, список файлов
     public partial class MainWindow : Window
     {
         public MainWindow()
         {
+            //worker.DirectoryChanged += DirectoryChangedHandler;
+            
             TexturesDirectory = Directory.GetCurrentDirectory() + "\\textures";
             InitializeComponent();
+            AddDrivesToList();
+            navigationBar.DirectoryChanged += DirectoryChangedHandler;
             AllocConsole();
             lstOfDisks.ItemsSource = ListOfDisks;
             //Directory.GetDirectories("C:\\").ToList().ForEach(p => Console.WriteLine(p));
 
         }
 
+        private void AddDrivesToList()
+        {
+            foreach (var drive in drives)
+            {
+                double f = (double)drive.AvailableFreeSpace / (double)drive.TotalSize;
+                Console.WriteLine(f);
+                _listOfDisks.Add(new Disk(drive.Name, 100 - f * 100));
+            }
+        }
+
+        private void DirectoryChangedHandler(object? sender, DirectoryChangedArgs e)
+        {
+            if (e.Path == "")
+            {
+                lstOfDisks.Visibility = Visibility.Visible;
+                lstOfDirectories.Visibility = Visibility.Collapsed;
+            }
+            else
+            {
+                lstOfDisks.Visibility = Visibility.Collapsed;
+                lstOfDirectories.Visibility = Visibility.Visible;
+            }
+            //Console.WriteLine(e.Path);
+        }
+
         private List<Disk> _listOfDisks = new List<Disk>();
 
         public List<Disk> ListOfDisks
         {
+            set
+            {
+                _listOfDisks = value;
+                lstOfDisks.ItemsSource = _listOfDisks;
+            }
+
             get
             {
-                foreach (var drive in drives)
-                {
-                    double f = (double)drive.AvailableFreeSpace / (double)drive.TotalSize;
-                    Console.WriteLine(f);
-                    _listOfDisks.Add(new Disk(drive.Name, 100 - f * 100));
-                }
                 return _listOfDisks;
             }
         }
@@ -77,8 +107,47 @@ namespace filemanager
                 this.name = name;
                 this.fullness = fullness;
                 this.pathOfImage = Directory.GetCurrentDirectory() + "\\textures\\disk_image.png";
+                pathOfImage = "C:\\Users\\Никита\\source\\repos\\filemanager\\filemanager\\textures\\disk_image.png";
             }
         }
+
+        public class MyDirectories
+        {
+            string _name = "";
+
+            string _path = "";
+
+            public string Name
+            {
+                get { return _name; }
+                set { _name = value; }
+            }
+
+            public string PathOfDirectory
+            {
+                get { return _path; }
+                set { _path = value; }
+            }
+
+            public MyDirectories(string name, string path)
+            {
+                Name = name;
+                PathOfDirectory = path;
+            }
+        }
+
+        private void ChangePathThruDisks(object sender, MouseButtonEventArgs e)
+        {
+            navigationBar.Line = ListOfDisks[lstOfDisks.SelectedIndex].Name;
+            lstOfDisks.SelectedIndex = -1;
+        }
+
+        private void ChangePathThruDirectory(object sender, MouseButtonEventArgs e)
+        {
+            //navigationBar.Line = ListOfDisks[lstOfDisks.SelectedIndex].Name;
+
+        }
+
     }
 
 
--- filemanager/NavigationBar.xaml.cs
+++ filemanager/NavigationBar.xaml.cs
@@ -29,28 +29,31 @@ namespace filemanager
             
         }
 
+        public event EventHandler<DirectoryChangedArgs> DirectoryChanged = (s, e) => { };
+
         private static string line = "";
 
         public string Line
         {
             get { return line; }
-            set { line = value; this.DataContext = this; }
+            set { line = value; this.DataContext = this; Console.WriteLine(1); filePathBar.Text = value; DirectoryChanged(this, new DirectoryChangedArgs(value)); }
         }
-
-        //public event EventHandler LineChanged;
-
-        
 
         private void searchingButton_Click(object sender, RoutedEventArgs e)
         {
-            Line = filePathBar.Text;
-
+            if (Line != filePathBar.Text) Line = filePathBar.Text;
         }
 
         [DllImport("kernel32.dll", SetLastError = true)]
         [return: MarshalAs(UnmanagedType.Bool)]
         static extern bool AllocConsole();
 
+    }
+
+    public class DirectoryChangedArgs
+    {
+        public string? Path { get; set; }
+        public DirectoryChangedArgs(string? path) { Path = path; }
     }
 
 }
